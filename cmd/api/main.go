3ae038fa3c80e80d5a5f6eaae04bc542096862ae
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-10-21 00:31:29 +0100

    Refactored directory structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env string
+}
+
+type application struct{
+	logger *log.Logger
+	config config
+}
+
 
 func main() {
-	fmt.Println("Hello Greenlight")
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "Set port value")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development/staging/environment)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+
+	app := &application{
+		logger: logger,
+		config: cfg,
+	}
+
+	// Multiplexer
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server {
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on port %d", cfg.env, cfg.port)
+	logger.Fatal(srv.ListenAndServe())
 }
