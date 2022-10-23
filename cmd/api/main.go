c8f86fdceb9de6ce2ba9ba47ad798c9530ffb084
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-10-23 03:42:36 +0100

    Setup database connection for PostgreSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env string
+	db struct {
+		dsn string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime string
+	}
 }
 
 type application struct{
@@ -21,15 +31,52 @@ type application struct{
 	config config
 }
 
+// openDB opens a connection pool
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	// custom config of the DB connection
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
 
 func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "Set port value")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development/staging/environment)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("LIGHTEN_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle connections time")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+	logger.Println("database connection pool established")
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
