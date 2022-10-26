bc054f5aab4ca1916e9a35748137eba1d96dc470
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-10-26 07:36:10 +0100

    feat: implemented a new user registration endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,11 +13,14 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowed)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheck)
+
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovies)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovie)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovie)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovie)
+
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUser)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
---
+++ internal/data/users.go
@@ -0,0 +1,188 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/lighten/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+type User struct {
+	ID int64 `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password password `json:"-"`
+	Activated bool `json:"activated"`
+	Version int `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash []byte
+}
+
+// Set() method calculates the bcrypt hash of a plaintext password, and stores both 
+// the hash and the plaintext versions in the struct.
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+
+	if err != nil {
+		return err
+	}
+
+	p.hash = hash
+	p.plaintext = &plaintextPassword
+
+	return nil
+}
+
+// Matches() method checks whether the provided plaintext password matches the 
+// hashed password stored in the struct, returning true if it matches and false
+// otherwise.
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+// ValidateEmail sanity-check the provided user's email
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+// ValidatePasswordPlaintext sanity-check the provided user's password
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+// ValidateUser sanity-check the provided user JSON
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+// Wraps the databse connection pool
+type UserModel struct {
+	DB *sql.DB
+}
+
+// Insert inserts a new user record into the users table.
+func (m UserModel) Insert(user *User) error {
+	stmt := `
+	INSERT INTO users (name, email, password_hash, activated) 
+	VALUES ($1, $2, $3, $4) 
+	RETURNING id, created_at, version`
+
+	args := []interface{}{user.Name, user.Email, user.Password.hash, user.Activated}
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+// GetByEmail retrieves a specific user record with the email
+func (m UserModel) GetByEmail(email string) (*User, error) {
+	stmt := `
+	SELECT id, created_at, name, email, password_hash, activated, version 
+	FROM users 
+	WHERE email = $1`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+// Update updates a record with the user args passed.
+func (m UserModel) Update(user *User) error {
+	stmt := `
+	UPDATE users 
+	SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1 
+	WHERE id = $5 AND version = $6
+	RETURNING version`
+
+	args := []interface{}{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, stmt, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "user_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrRecordNotFound
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
