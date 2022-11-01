039ddd2bc69c0ea1c9012ef499640da9c3f49522
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-11-01 11:12:21 +0100

    Implemented metric-monitoring on request-response level

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/lighten/internal/data"
 	"github.com/lighten/internal/validator"
 	"golang.org/x/time/rate"
@@ -208,5 +211,23 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+// metrics specific request-response metrics for monitoring.
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestsReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_μs")
+	totalResponsesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		totalRequestsReceived.Add(1)
+
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
+
+		totalResponsesSent.Add(1)
+		totalProcessingTimeMicroseconds.Add(metrics.Duration.Microseconds())
+		totalResponsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
 	})
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/lib/pq v1.10.0
 
+require github.com/felixge/httpsnoop v1.0.1 // indirect
+
 require (
 	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	golang.org/x/crypto v0.1.0
